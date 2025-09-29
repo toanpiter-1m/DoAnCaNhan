3b4cad1f36b640be132a8d01ad6b1b38160c487a
toanpiter-1m <anhtoan1526@gmail.com>
2025-09-29 21:21:01 +0700

    Add files via upload

---
+++ DACN.java
@@ -0,0 +1,12 @@
+
+package dacn;
+
+public class DACN {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFStudent jfs = new JFStudent();
+        jfs.show();
+    }
+    
+}
---
+++ Student.java
@@ -0,0 +1,63 @@
+
+package dacn;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+public class Student implements Serializable {
+    private String id;
+    private String name;
+    private Date birthday;
+    private String address;
+    private String email;
+
+    public Student(String id, String name, Date birthday, String address, String email) {
+        this.id = id;
+        this.name = name;
+        this.birthday = birthday;
+        this.address = address;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
